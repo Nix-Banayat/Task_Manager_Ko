3e9293cbef778c274c4f6c825191f613a5bc6f9e
Nix-Banayat <164652107+Nix-Banayat@users.noreply.github.com>
2024-07-13 23:32:00 +0800

    Create Task.cs

---
+++ Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Manager_Ko
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Task(string title, string description, string priority)
+        {
+            Title = title;
+            Description = description;
+            Priority = priority;
+            IsCompleted = false;
+        }
+    }
+}
