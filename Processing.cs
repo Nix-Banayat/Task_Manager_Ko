0d0b29755ed30585d1f6e69ebbfdf8b845fbe1f2
Nix-Banayat <164652107+Nix-Banayat@users.noreply.github.com>
2024-07-13 23:31:54 +0800

    Create Processing.cs

---
+++ Processing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Manager_Ko
+{
+    internal class Processing
+    {
+        public static void Options(int option)
+        {
+            switch (option)
+            {
+                case 1:
+                    Compiler.AddTaskCompile();
+                    break;
+                case 2:
+                    Compiler.ViewTasks();
+                    break;
+                case 3:
+                    Compiler.EditTasksCompile();
+                    break;
+                case 4:
+                    Compiler.DeleteTaskCompile();
+                    break;
+                case 5:
+                    Compiler.MarkTaskAsCompletedCompile();
+                    break;
+                case 6:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    TaskManage.Arrange();
+                    break;
+            }
+        }
+    }
+}
